e6ddf7c0e3110dc8df76827b9c049d79adb61a47
sergiopoliveira <sergiopoliveira@outlook.com>
2018-06-13 10:30:48 +0100

    CRUD Database - Get Customers

---
+++ src/main/java/com/sergio/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,35 @@
+package com.sergio.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sergio.springdemo.entity.Customer;
+import com.sergio.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	// autowire the CustomerService
+	@Autowired
+	CustomerService customerService;
+	
+	// add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		
+		return customerService.getCustomers();
+	}
+	
+	// add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		return customerService.getCustomer(customerId);
+	}
+}
